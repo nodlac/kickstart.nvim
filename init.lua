c07bfd6d066e96a9a622798b2b15cf259c32ded4
nodlac <caldonpreece@gmail.com>
2024-12-03 11:49:50 -0700

    Switched to eslint-d as primary

--- init.lua
+++ init.lua
@@ -686,7 +686,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        javascript = { 'eslint', stop_after_first = true },
+        javascript = { 'eslint-d', 'prettier', stop_after_first = true },
       },
     },
   },
